a7dae2692524aaf42d643d1a1b9e3ec556740898
Clinton Gormley <clinton@iannounce.co.uk>
2012-01-23 14:12:59 +0100

    Model can now load index aliases in case from the live cluster

--- lib/ESModel/Role/Model.pm
+++ lib/ESModel/Role/Model.pm
@@ -19,13 +19,40 @@ has '_index_cache' => (
     default => sub { {} },
     handles => {
         _get_index   => 'get',
-        _cache_index => 'set'
+        _cache_index => 'set',
     },
+);
+
+has '_live_indices' => (
+    isa     => 'HashRef',
+    is      => 'ro',
+    traits  => ['Hash'],
+    builder => '_update_live_indices',
+    clearer => '_clear_live_indices',
+    lazy    => 1,
+    handles => { _live_index => 'get', },
+
 );
 
 #===================================
 sub _build_es { ElasticSearch->new }
 #===================================
+
+#===================================
+sub _update_live_indices {
+#===================================
+    my $self    = shift;
+    my $meta    = $self->meta;
+    my $indices = $self->es->get_aliases( index => [ $meta->all_indices ] )
+        ->{indices};
+
+    my %live;
+    while ( my ( $name, $aliases ) = each %$indices ) {
+        ( $live{$name} ) = grep { $self->index($_) }
+            grep { $meta->has_index($_) } @$aliases;
+    }
+    \%live;
+}
 
 #===================================
 sub index {
@@ -35,17 +62,20 @@ sub index {
 
     my $index = $self->_get_index($name);
     unless ($index) {
-
-        my $opts = $self->meta->index($name)
-            or croak "Unknow index name '$name'";
-
+        my $opts = $self->meta->index($name);
+        unless ($opts) {
+            my $live_index
+                = $self->_live_index($name)
+                || $self->_clear_live_indices && $self->_live_index($name)
+                || croak "Unknown index name '$name'";
+            $opts = $self->meta->index($live_index);
+        }
         $index = ESModel::Index->new(
             name  => $name,
             model => $self,
             %$opts
         );
-
-        $self->_cache_index($name => $index);
+        $self->_cache_index( $name => $index );
     }
     return $index;
 }
