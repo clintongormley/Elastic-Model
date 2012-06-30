55a3742c8593cb7cbd8fc6b14085ecd6d8e9554e
Clinton Gormley <clinton@iannounce.co.uk>
2012-06-30 16:25:15 +0200

    Added docs for Elastic::Model::Store

--- lib/Elastic/Model/Store.pm
+++ lib/Elastic/Model/Store.pm
@@ -15,3 +15,22 @@ __END__
 This is an empty class which provides the default implementation of
 the ElasticSearch backend as implemented in L<Elastic::Model::Role::Store>.
 
+=head1 IMPORTED ATTRIBUTES
+
+=head2 L<es|Elastic::Model::Role::Store/es>
+
+=head1 IMPORTED METHODS
+
+=head2 L<get_doc()|Elastic::Model::Role::Store/get_doc()>
+
+=head2 L<doc_exists()|Elastic::Model::Role::Store/doc_exists()>
+
+=head2 L<create_doc()|Elastic::Model::Role::Store/create_doc()>
+
+=head2 L<index_doc()|Elastic::Model::Role::Store/index_doc()>
+
+=head2 L<delete_doc()|Elastic::Model::Role::Store/delete_doc()>
+
+=head2 L<search()|Elastic::Model::Role::Store/search()>
+
+=head2 L<scrolled_search()|Elastic::Model::Role::Store/scrolled_search()>
